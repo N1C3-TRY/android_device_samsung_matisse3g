cc0f9b47bdd09e3141e2e438a0775047c8ee64e1
sub77 <sub77@ymail.com>
2016-11-13 03:42:31 +0000

    klte -> matisse3g
    Change-Id: Ibad18d566b3186aee49dd9b6bd2126ad62cf67b4

--- cm.mk
+++ cm.mk
@@ -1,8 +1,30 @@
+# Copyright (C) 2014 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+# Inherit from matisse3g device
+$(call inherit-product, device/samsung/matisse3g/device.mk)
 
-$(call inherit-product, device/samsung/klte/full_klte.mk)
+# Set those variables here to overwrite the inherited values.
+PRODUCT_NAME := cm_matisse3g
+PRODUCT_DEVICE := matisse3g
+PRODUCT_BRAND := samsung
+PRODUCT_MANUFACTURER := samsung
+PRODUCT_MODEL := T531
 
-PRODUCT_DEVICE := klte
-PRODUCT_NAME := cm_klte
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=samsung/matisse3gxx/matisse3g:5.0.2/LRX22G/T531XXU1BOD8:user/release-keys PRIVATE_BUILD_DESC="matisse3gxx-user 5.0.2 LRX22G T531XXU1BOD8 release-keys"
